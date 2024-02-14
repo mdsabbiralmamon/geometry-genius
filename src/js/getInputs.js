76f430c2c939526f40d0e5373b365e9eba628937
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-02-14 14:40:08 +0600

    triangle calculation done

---
+++ src/js/getInputs.js
@@ -0,0 +1,4 @@
+function getInput(inputID){
+    const input = parseFloat(document.getElementById(inputID).value);
+    return input;
+}
---
+++ src/js/triangle.js
@@ -0,0 +1,6 @@
+function calculateTriangleArea(){
+    const base = getInput('tri-base');
+    const height = getInput('tri-height');
+    const area = .5*base*height;
+    return area;
+}
